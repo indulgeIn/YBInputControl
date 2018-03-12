b1d37e93dec2aee513cd85c8a3828c44f2341111
杨少 <yangshao@yangshaodemac-mini.local>
2018-03-12 18:03:30 +0800

    d

--- YBInputLimitDemo/YBInputLimitDemo/ViewController.m
+++ YBInputLimitDemo/YBInputLimitDemo/ViewController.m
@@ -8,8 +8,10 @@
 
 #import "ViewController.h"
 #import "UIView+YBInputLimit.h"
+#import "UIView+YBInputControl.h"
 
-@interface ViewController ()
+
+@interface ViewController () <UITextFieldDelegate>
 
 @end
 
@@ -23,56 +25,38 @@
     textfield.frame = CGRectMake(20, 100, 300, 50);
     [self.view addSubview:textfield];
     
-    
     //* 注意1：由于点击联想输入在输入之前无法监听，也就无法精确控制输入字符，所以如果不是输入描述性语言的情况下，建议把联想输入关闭
-    textfield.autocorrectionType = UITextAutocorrectionTypeNo;
+//    textfield.autocorrectionType = UITextAutocorrectionTypeNo;
     //* 注意2：由于中文输入状态自带联想，所以如果不用输入中文，请设置键盘让用户无法切换到中文输入状态
-    textfield.keyboardType = UIKeyboardTypeASCIICapable;
+//    textfield.keyboardType = UIKeyboardTypeASCIICapable;
     
     
-    //* 一句代码实现
-    [textfield setYBInputLimit:YBInputLimitModel
-     .initialization
-     .setInputLimitType(YBInputLimitType_numbers|YBInputLimitType_lettersSmall)
-     .setMaxLength(10)
-     .setTextChanged(^(UITextField *observe){
-
-        NSLog(@"textchange : %@", observe.text);
-    })];
-    
-//    //* 常规实现
-//    YBInputLimitModel *model = [YBInputLimitModel new];
-//    model.inputLimitType = YBInputLimitType_numbers|YBInputLimitType_lettersSmall;
-//    model.maxLength = 10;
-//    [model setTextChanged:^(id observe){
-//        
+//    YBInputControlProfile *profile = [YBInputControlProfile new];
+//    [profile setTextChanged:^(id  _Nonnull observer) {
+//        NSLog(@"%@", [observer valueForKey:@"text"]);
 //    }];
-//    [textfield setYBInputLimit:model];
-//    
-//    
-//    //* 不使用block
-//    [textfield setYBInputLimit:YBInputLimitModel
-//     .initialization
-//     .setInputLimitType(YBInputLimitType_numbers|YBInputLimitType_lettersSmall)
-//     .setMaxLength(10)
-//     .addTargetAndAction(self, @selector(sel0:))];
-//    
-//    
-//    //* 不使用block
-//    YBInputLimitModel *model = [YBInputLimitModel new];
-//    model.inputLimitType = YBInputLimitType_numbers|YBInputLimitType_lettersSmall;
-//    model.maxLength = 10;
-//    [model addTargetOfTextChange:self action:@selector(sel0:)];
-//    [textfield setYBInputLimit:model];
-//    
-//    
-//    //* 直接输入正则
-//    [textfield setYBInputLimit:YBInputLimitModel.initialization.setRegularStr(@"^[0-9]+$")];
+//    [profile addTargetOfTextChange:self action:@selector(sel0:)];
+    
+//    textfield.yb_inputCP = YBInputControlProfile.creat.set_maxLength(4).set_textControlType(YBTextControlType_numbers).set_textChanged(^(id obj){
+//        NSLog(@"%@", [obj valueForKey:@"text"]);
+//    });
+    
+    textfield.yb_inputCP = YBInputControlProfile.creat.set_maxLength(10).set_textControlType(YBTextControlType_lettersBig).set_targetOfTextChange(self, @selector(sel0:));
+    textfield.delegate = self;
+    
+    
+
 }
 
-- (void)sel0:(id)target {
-    UITextField *tf = (UITextField *)target;
-    NSLog(@"sel0 : %@", tf.text);
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    NSLog(@"vc: %@", NSStringFromSelector(_cmd));
+    return YES;
+}
+
+
+
+- (void)sel0:(id)sender {
+    NSLog(@"sel0 : %@", [sender valueForKey:@"text"]);
 }
 
 
